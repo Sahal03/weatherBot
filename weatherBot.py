acdc9d8785970adfe3427262d54ae219ff0021c9
Sahal A <sahalaidid@gmail.com>
2021-12-23 04:03:43 -0500

    Update weatherBot.py

--- weatherBot.py
+++ weatherBot.py
@@ -6,9 +6,10 @@ bot = discord.Client()
 @bot.event
 async def on_ready():
 #...
-
+    #assign channel id
     gen = bot.get_channel(804427366859276340)
 
+    #send generic weather message to gen
     await gen.send("How about this weather?")
 # Run bot
 bot.run("OTIzNDk1NTk5MTE3MDc0NTAy.YcQ2TA.2GjGyKaaRfV4notmRcnpZeJI3Bs")
