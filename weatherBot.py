4bb5a7be49778402de9a1f22cf052bcaf598faf2
Sahal A <96517902+Sahal03@users.noreply.github.com>
2021-12-26 01:23:20 -0500

    Add files via upload

---
+++ weatherBot.py
@@ -0,0 +1,47 @@
+import discord, time, requests, os
+from discord.ext import commands
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+cityName = "Toronto,CA"
+APIKEY = os.getenv("APIKEY")
+TOKEN = os.getenv("TOKEN")
+
+def getWeather(cityName,apiKey):
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
+        info = requests.get(url).json()
+        temp = int(info['main']['temp'] -273.15)
+        return temp
+    
+
+
+
+# Create discord bot and set command prefix
+bot = commands.Bot(command_prefix='!')
+
+#create weather command
+@bot.command(name='weather') 
+async def getCity():
+        msg = await bot.wait_for("weather", check=requests.check_compatibility)
+        return msg
+async def weather(context):
+    city = getCity()
+    y = str(getWeather(city,APIKEY))
+    x = y + "°"
+    myEmbed = discord.Embed(title="Weather",description=x)
+    
+    await context.message.channel.send(embed=myEmbed)
+
+@bot.command(name='commands')
+async def help(context):
+    x = "Command displays the temperature of a given city in celcius\n!weather [CITY][COUNTRY ABREVIATION]"
+    myEmbed = discord.Embed(title="!weather",description=x)
+    
+    await context.message.channel.send(embed=myEmbed)
+
+# Run bot
+bot.run(TOKEN)
+
+
