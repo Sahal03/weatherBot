a26aff6f120a5edc922513b8a95dd68f78fb83da
Sahal A <sahalaidid@gmail.com>
2021-12-25 21:09:42 -0500

    Update weatherBot.py

--- weatherBot.py
+++ weatherBot.py
@@ -1,4 +1,4 @@
-import discord
+import discord, time
 
 # Create discord client
 bot = discord.Client()
@@ -8,10 +8,11 @@ async def on_ready():
 #...
     #assign channel id
     gen = bot.get_channel(804427366859276340)
-
+    for _ in range(5):
     #send generic weather message to gen
-    await gen.send("How about this weather?")
+        await gen.send("IT'S GON RAIN")
+        time.sleep(5)
 # Run bot
-bot.run("OTIzNDk1NTk5MTE3MDc0NTAy.YcQ2TA.2GjGyKaaRfV4notmRcnpZeJI3Bs")
+bot.run("OTIzNDk1NTk5MTE3MDc0NTAy.YcQ2TA.kXRniVYxy0PC_JH34JkSlkYqJfQ")
 
 
