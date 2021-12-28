c25f65f1f52be9e9e2dcb339d598cf1fdc1adb7c
Sahal A <sahalaidid@gmail.com>
2021-12-27 21:29:07 -0500

    weatherBotV1
    just need to fully comment it

--- weatherBot.py
+++ weatherBot.py
@@ -1,12 +1,12 @@
-import discord, time, requests, os, json
+import discord, requests, os
 from discord.ext import commands
 
 from dotenv import load_dotenv
+from requests.api import get
 load_dotenv()
 
 
-#gotta figure out userInput so the city isn't manual
-cityName = "Toronto,CA"
+#call the apikey and token in to weatherBot.py
 APIKEY = os.getenv("APIKEY")
 TOKEN = os.getenv("TOKEN")
 
@@ -15,47 +15,89 @@ def getTemp(cityName,apiKey):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
     info = requests.get(url).json()
     temp = int(info['main']['temp'] -273.15)
-
-    tempString = info['weather'][0]
-    print(tempString)
-    print(type(tempString))
-    print(tempString['description'])
+    
     return temp
 
 #getWeather method uses the api to get the weather description
 def getWeather(cityName,apiKey):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
     info = requests.get(url).json()
-
     tempDic = info['weather'][0]
     desc = tempDic['description']
-
     return desc
 
+#gets the icon code for the emoji and returns the url
+def getEmoji(cityName,apiKey):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
+    info = requests.get(url).json()
+    tempDic = info['weather'][0]
+    weather_icon = tempDic['icon']
+    url = "http://openweathermap.org/img/wn/" + weather_icon + "@2x.png"
+
+    return url
+
+#gets and returns the "feels like" temp
+def getFeel(cityName,apiKey):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
+    info = requests.get(url).json()
+    temp = int(info['main']['feels_like'] -273.15)
+    
+    return temp
+
+#gets and returns the humidity 
+def getHumidity(cityName,apiKey):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
+    info = requests.get(url).json()
+    temp = int(info['main']['humidity'])
+    
+    return temp
 
 # Create discord bot and set command prefix
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', help_command=None)
 
 #create weather command
 @bot.command(name='weather') 
-async def weather(context):
-    city = cityName
+async def weather(ctx, arg):
+
+    #user enters a city
+    city = arg
+    #call the getEmoji function
+    emoji = getEmoji(city,APIKEY)
+    #call the getHumidity function and cast to string
+    humidity = str(getHumidity(city,APIKEY)) + "%"
+    #call the getFeel function and cast to string
+    feel = str(getFeel(city,APIKEY)) + "°" 
+    #call the getTemp function and cast to string
     y = str(getTemp(city,APIKEY))
-    z = getWeather(city,APIKEY)
-    x = y + "°" + " with " + z
-    myEmbed = discord.Embed(title="Weather",description=x)
+    #call the getWeather function
+    z = (getWeather(city,APIKEY))
+    #weather description
+    x = y + "°" + " with " + z 
+
     
-    await context.message.channel.send(embed=myEmbed)
+    myEmbed = discord.Embed(title="Weather in " + city.upper(),description=x)
+    myEmbed.set_author(name="Weather Bot")
+    myEmbed.set_thumbnail(url=emoji)
+    myEmbed.add_field(name='Humidity',value=humidity,inline=True)
+    myEmbed.add_field(name='Feels Like',value=feel,inline=True)
+
+    await ctx.message.channel.send(embed=myEmbed)
 
 
-@bot.command(name='commands')
-async def help(context):
-    x = "Command displays the temperature of a given city in celcius\n!weather [CITY][COUNTRY ABREVIATION]"
-    myEmbed = discord.Embed(title="!weather",description=x)
+@bot.command(name='help')
+async def help(ctx):
+    x = "!weather [CITY][COUNTRY ABREVIATION]"
+    y = "!weather [CITY]"
+    myEmbed = discord.Embed(title="Weather Bot",description=x)
+    myEmbed.add_field(name="or",value=y,inline=True)
     
-    await context.message.channel.send(embed=myEmbed)
+    await ctx.message.channel.send(embed=myEmbed)
+
+
+#
+@weather.error
+async def error(ctx,error):
+     await ctx.message.channel.send('Try the "!help" command')
 
 # Run bot
 bot.run(TOKEN)
-
-
